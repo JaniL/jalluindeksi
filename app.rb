8446e4fd2d8b422d804a4720ccd1104788e3df28
Jani Luukko <jj.luukko@gmail.com>
2016-01-13 22:15:59 +0200

    heh

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
+require "net/http"
+require "uri"
+require 'rubygems'
+require 'json'
+
 get '/' do
-  "Sinatra Heroku Cedar Template - The bare minimum for a sinatra app on cedar, running rack, and using bundler."
+  uri = URI.parse("http://www.alko.fi/api/find/products?Language=fi&Page=0&PageSize=1&ProductIds=000706&SingleGrape=false&Sort=0")
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+
+  response = http.request(request)
+  json = JSON.parse(response.body)
+  "Jalluindeksi on tällä hetkellä #{json["Results"][0]["Price"]} euroa."
 end
