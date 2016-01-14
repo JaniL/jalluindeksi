ed39e61984cdd504d171f66d9b18dee1cf5fed0b
Jani Luukko <jj.luukko@gmail.com>
2016-01-14 18:18:00 +0200

    heh

--- app.rb
+++ app.rb
@@ -4,11 +4,22 @@ require 'rubygems'
 require 'json'
 
 get '/' do
+  #uri = URI.parse("http://www.alko.fi/api/find/products?Language=fi&Page=0&PageSize=1&ProductIds=000706&SingleGrape=false&Sort=0")
+  #http = Net::HTTP.new(uri.host, uri.port)
+  #request = Net::HTTP::Get.new(uri.request_uri)
+
+  #response = http.request(request)
+  #json = JSON.parse(response.body)
+  #{}"Jalluindeksi on tällä hetkellä #{json["Results"][0]["Price"]} euroa."
+  erb :index
+end
+
+get '/price' do
   uri = URI.parse("http://www.alko.fi/api/find/products?Language=fi&Page=0&PageSize=1&ProductIds=000706&SingleGrape=false&Sort=0")
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Get.new(uri.request_uri)
 
   response = http.request(request)
   json = JSON.parse(response.body)
-  "Jalluindeksi on tällä hetkellä #{json["Results"][0]["Price"]} euroa."
+  json["Results"][0]["Price"]
 end
